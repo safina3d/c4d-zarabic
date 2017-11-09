d347ec5fe77df098acbe3495ad775e33238f5be9
zak <zak@me.com>
2017-11-09 08:53:38 +0100

    Dev branch
    Test init

---
+++ Dev/arabic.py
@@ -0,0 +1,338 @@
+# -*- coding: utf-8 -*-
+
+
+class LetterShape:
+
+    ISOLATED = 0
+    INITIAL = 2
+    MIDDLE = 3
+    FINAL = 1
+
+    def __init__(self): pass
+
+
+class LinkType:
+
+    WITHOUT = 0
+    BEFORE = 1
+    BOTH = 2
+    EXTENSION = 3
+
+    def __init__(self): pass
+
+
+class AbjadData:
+
+    HAMZA_DECIMAL_VALUE = 1569
+
+    LAM_DECIMAL_VALUE = 1604
+
+    FATHATAN_DECIMAL_VALUE = 1611
+
+    SHADDA_DECIMAL_VALUE = 1617
+
+    SOUKOUN_DECIMAL_VALUE = 1618
+
+    LINK_TYPE_LIST = [
+        LinkType.WITHOUT, LinkType.BEFORE, LinkType.BEFORE, LinkType.BEFORE, LinkType.BEFORE,
+        LinkType.BOTH, LinkType.BEFORE, LinkType.BOTH, LinkType.BEFORE, LinkType.BOTH, LinkType.BOTH,
+        LinkType.BOTH, LinkType.BOTH, LinkType.BOTH, LinkType.BEFORE, LinkType.BEFORE, LinkType.BEFORE,
+        LinkType.BEFORE, LinkType.BOTH, LinkType.BOTH, LinkType.BOTH, LinkType.BOTH, LinkType.BOTH,
+        LinkType.BOTH, LinkType.BOTH, LinkType.BOTH, LinkType.WITHOUT, LinkType.WITHOUT, LinkType.WITHOUT,
+        LinkType.WITHOUT, LinkType.WITHOUT, LinkType.EXTENSION, LinkType.BOTH, LinkType.BOTH, LinkType.BOTH,
+        LinkType.BOTH, LinkType.BOTH, LinkType.BOTH, LinkType.BOTH, LinkType.BEFORE, LinkType.BOTH,
+        LinkType.BOTH, LinkType.BEFORE, LinkType.BEFORE, LinkType.BEFORE, LinkType.BEFORE,
+        LinkType.BEFORE, LinkType.BEFORE, LinkType.BEFORE, LinkType.BEFORE
+    ]
+
+    ARABIC_FORM_B_VALUES = [
+        65152, 65153, 65155, 65157, 65159, 65161, 65165, 65167, 65171, 65173,
+        65177, 65181, 65185, 65189, 65193, 65195, 65197, 65199, 65201, 65205,
+        65209, 65213, 65217, 65221, 65225, 65229, 1595, 1596, 1597, 1598,
+        1599, 1600, 65233, 65237, 65241, 65245, 65249, 65253, 65257, 65261,
+        65263, 65265, 65136, 65138, 65140, 65142, 65144, 65146, 65148, 65150
+    ]
+
+    ALIF_VALUES = [1570, 1571, 1573, 1575]
+
+    LAM_ALIF_VALUES = [None, 65269, 65271, None, 65273, None, 65275]
+
+    SHADDA_VALUES = [64608, 64606, 64607, 64608, 64609, 64610, 64754, 64755, 64756]
+
+    def __init__(self): pass
+
+
+class Character:
+
+    def __init__(self, character):
+        self.character = character
+        self.decimal_value = ord(character)
+        self.is_arabic_character = False
+        self.shape = LetterShape.ISOLATED
+        self.link_type = LinkType.WITHOUT
+        self.offset = None
+        self.__update_char_values()
+
+    def __update_char_values(self):
+        """ private: update the default values if the passed current character is arabic """
+        self.is_arabic_character = AbjadData.HAMZA_DECIMAL_VALUE <= self.decimal_value <= AbjadData.SOUKOUN_DECIMAL_VALUE
+
+        if self.is_arabic_character:
+            self.offset = self.decimal_value - AbjadData.HAMZA_DECIMAL_VALUE
+            self.link_type = AbjadData.LINK_TYPE_LIST[self.offset]
+
+    def is_special_char(self):
+        return not (self.character.isalnum() or self.is_tachkil())
+
+    def is_alif(self):
+        return self.decimal_value in AbjadData.ALIF_VALUES
+
+    def is_hamza(self):
+        return self.decimal_value == AbjadData.HAMZA_DECIMAL_VALUE
+
+    def is_lam(self):
+        return self.decimal_value == AbjadData.LAM_DECIMAL_VALUE
+
+    def is_tachkil(self):
+        return AbjadData.FATHATAN_DECIMAL_VALUE <= self.decimal_value <= AbjadData.SOUKOUN_DECIMAL_VALUE
+
+    def is_shadda(self):
+        return self.decimal_value == AbjadData.SHADDA_DECIMAL_VALUE
+
+    def can_be_connected_to(self, other_char):
+        return self.link_type != LinkType.WITHOUT and (other_char.link_type == LinkType.BOTH or other_char.link_type == LinkType.EXTENSION)
+
+    def transform_char(self):
+
+        if self.is_special_char():
+            return self.flip_special_char()
+
+        if self.is_arabic_character:
+
+            decimal_value = AbjadData.ARABIC_FORM_B_VALUES[self.offset]
+
+            if self.link_type == LinkType.WITHOUT:
+                return unichr(decimal_value)
+            if self.link_type == LinkType.BEFORE:
+                return unichr(decimal_value + self.shape % 2)
+            if self.link_type == LinkType.BOTH:
+                return unichr(decimal_value + self.shape)
+
+        return self.character
+
+    def transform_lam_alif_char(self):
+        decimal_value = AbjadData.LAM_ALIF_VALUES[self.offset] + self.shape % 2
+        return unichr(decimal_value)
+
+    def transform_shadda_char(self):
+        index = self.offset % 42 + 3 * (self.shape % 2)
+        if index < len(AbjadData.SHADDA_VALUES):
+            return unichr(AbjadData.SHADDA_VALUES[index])
+        return unichr(65148 + self.shape % 2)
+
+    def flip_special_char(self):
+        """ Returns the opposite character """
+        if self.character == '(':
+            return ')'
+        if self.character == ')':
+            return '('
+        if self.character == '{':
+            return '}'
+        if self.character == '}':
+            return '{'
+        if self.character == '[':
+            return ']'
+        if self.character == ']':
+            return '['
+        if self.character == '<':
+            return '>'
+        if self.character == '>':
+            return '<'
+        if self.character == ',':
+            return unichr(1548)
+        if self.character == ';':
+            return unichr(1563)
+        if self.character == '?':
+            return unichr(1567)
+        if self.character == unichr(171):
+            return unichr(187)
+        if self.character == unichr(187):
+            return unichr(171)
+        return self.character
+
+
+class Text:
+
+    def __init__(self, user_text):
+        self.user_text = user_text.decode('utf-8')
+
+    def get_word_list(self):
+        """ Split the given text into words """
+        word_list = []
+        current_word = ''
+        for c in self.user_text:
+            char = Character(c)
+            if char.is_special_char() and not char.is_tachkil():
+                if current_word:
+                    word_list.append(current_word)
+                    current_word = ''
+                word_list.append(c)
+            else:
+                current_word += c
+        if current_word:
+            word_list.append(current_word)
+        return word_list
+
+    @staticmethod
+    def is_last_letter(word, index):
+        return Text.get_next_letter(word, index)[0] is None
+
+    @staticmethod
+    def is_shadda_context(current_char, next_char):
+        if next_char is None:
+            return False
+        return current_char.is_shadda() and next_char.is_tachkil() or current_char.is_tachkil() and next_char.is_shadda()
+
+    @staticmethod
+    def is_lam_alif_context(current_char, next_letter):
+        if next_letter is None:
+            return False
+        return current_char.is_lam() and next_letter.is_alif()
+
+    @staticmethod
+    def get_next_char(word, index):
+        word_length = len(word)
+        current_index = index + 1
+        return Character(word[current_index]) if 0 <= current_index < word_length else None
+
+    @staticmethod
+    def get_previous_char(word, index):
+        word_length = len(word)
+        current_index = index - 1
+        return Character(word[current_index]) if 0 <= current_index < word_length else None
+
+    @staticmethod
+    def get_next_letter(word, index):
+        word_length = len(word)
+        current_index = index + 1
+        while current_index < word_length:
+            current_char = Character(word[current_index])
+            if not current_char.is_tachkil():
+                return current_char, current_index
+            current_index += 1
+        return None, None
+
+    @staticmethod
+    def get_previous_letter(word, index):
+        word_length = len(word)
+        if word_length == 0:
+            return None
+        current_index = index - 1
+        while current_index >= 0:
+            current_char = Character(word[current_index])
+            if not current_char.is_tachkil():
+                return current_char, current_index
+            current_index -= 1
+        return None, None
+
+    @staticmethod
+    def transform_word(word):
+
+        result_word = ''
+        word_length = len(word)
+        last_char_index = word_length - 1
+        index = 0
+
+        while index < word_length:
+
+            previous_char = Text.get_previous_char(word, index)
+            current_char = Character(word[index])
+            next_char = Text.get_next_char(word, index)
+
+            previous_letter, previous_letter_index = Text.get_previous_letter(word, index)
+            next_letter, next_letter_index = Text.get_next_letter(word, index)
+
+            next_index = index + 1
+
+            if not current_char.is_arabic_character and not current_char.is_special_char():
+                return word
+
+            if Text.is_lam_alif_context(current_char, next_letter):
+                    if index == 0:
+                        next_letter.shape = LetterShape.INITIAL
+                    else:
+                        if current_char.can_be_connected_to(previous_letter):
+                            next_letter.shape = LetterShape.MIDDLE
+                        else:
+                            next_letter.shape = LetterShape.INITIAL
+                    result_word += next_letter.transform_lam_alif_char()
+                    index += (next_letter_index - index) + 1
+                    continue
+
+            if Text.is_shadda_context(current_char, next_char):
+                tachkil_char = next_char if current_char.is_shadda() else current_char
+                if previous_char and current_char.can_be_connected_to(previous_char) and next_index < last_char_index:
+                    tachkil_char.shape = LetterShape.MIDDLE
+                result_word += tachkil_char.transform_shadda_char()
+                index += 2
+                continue
+
+            # Tachkil
+            if current_char.is_tachkil():
+                if current_char.can_be_connected_to(previous_char) and index != last_char_index:
+                    current_char.shape = LetterShape.MIDDLE
+            else:
+                if index == 0:
+                    if not Text.is_last_letter(word, index):
+                        current_char.shape = LetterShape.INITIAL
+                elif 0 < index < last_char_index:
+                    if Text.is_last_letter(word, index) or (next_letter and next_letter.is_hamza()):
+                        if current_char.can_be_connected_to(previous_letter):
+                            current_char.shape = LetterShape.FINAL
+                    else:
+                        if current_char.can_be_connected_to(previous_letter):
+                            current_char.shape = LetterShape.MIDDLE
+                        else:
+                            current_char.shape = LetterShape.INITIAL
+                else:
+                    if current_char.can_be_connected_to(previous_letter):
+                        current_char.shape = LetterShape.FINAL
+
+            result_word += current_char.transform_char()
+            index += 1
+
+        # Lafdh Al Jalala
+        if result_word == u'ﺍﻟﻠﻪ':
+            result_word = u'ﷲ'
+
+        return result_word[::-1]
+
+    def transform_text(self):
+        word_list = self.get_word_list()
+        word_list.reverse()
+        result_text = ''
+        for word in word_list:
+            result_text += Text.transform_word(word)
+        return result_text
+
+    def text_to_unicode_string(self):
+        ucn = ''
+        for char in self.user_text:
+            _0x_value = hex(ord(char))
+            hex_value = _0x_value[2:]
+            zeros = (4 - len(hex_value)) * '0'
+            ucn += '\u' + zeros + hex_value
+        return ucn
+
+
+if __name__ == '__main__':
+
+    original_text = 'بسم الله الرحمن الرحيم'
+    result = ''
+
+    text = Text(original_text)
+    result = text.transform_text()
+    result = result.strip().encode('utf-8')
+
+    print result
+    # print Text(result.encode('utf-8')).text_to_unicode_string()
---
+++ Dev/test-zarabic.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from arabic import Character, LetterShape, LinkType, Text
+
+
+class TestCharacter(unittest.TestCase):
+
+    def test_init(self):
+        # Non Arabic char
+        txt = 'A'.decode('utf-8')
+        char = Character(txt)
+
+        self.assertEquals(char.decimal_value, 65)
+        self.assertFalse(char.is_arabic_character)
+        self.assertEquals(char.shape, LetterShape.ISOLATED)
+        self.assertEquals(char.link_type, LinkType.WITHOUT)
+        self.assertEquals(char.offset, None)
+
+        # Arabic char
+        txt = 'م'.decode('utf-8')
+        char = Character(txt)
+
+        self.assertEquals(char.decimal_value, 1605)
+        self.assertTrue(char.is_arabic_character)
+        self.assertEquals(char.shape, LetterShape.ISOLATED)
+        self.assertEquals(char.link_type, LinkType.BOTH)
+        self.assertEquals(char.offset, 36)
+
+    def test_is_special_char(self):
+        raw = '?@<>%*=&#¨ ^/|\\~`_-«»[]{}'
+        txt = raw.decode('utf-8')
+        for c in txt:
+            char = Character(c)
+            self.assertTrue(char.is_special_char())
+
+        raw = 'ءآأؤإئابةتثجحخدذرزسشصضطعظغػؼؽؾؿـفكقكلمنهوىي'
+        txt = raw.decode('utf-8')
+        for c in txt:
+            char = Character(c)
+            self.assertFalse(char.is_special_char())
+
+        raw = 'ًٌٍَُِّْ'
+        txt = raw.decode('utf-8')
+        for c in txt:
+            char = Character(c)
+            self.assertFalse(char.is_special_char())
+
+    def test_is_alif(self):
+        raw = 'آأإا'
+        txt = raw.decode('utf-8')
+        for c in txt:
+            char = Character(c)
+            self.assertTrue(char.is_alif())
+
+        raw = 'ةتثجحخدذرزسشصضطعظغػؼؽؾؿـفكقكلمنهوىي'
+        txt = raw.decode('utf-8')
+        for c in txt:
+            char = Character(c)
+            self.assertFalse(char.is_alif())
+
+    def test_is_lam(self):
+        raw = 'ل'
+        txt = raw.decode('utf-8')
+        for c in txt:
+            char = Character(c)
+            self.assertTrue(char.is_lam())
+
+    def test_is_tachkil(self):
+        raw = 'ًٌٍَُِّْ'
+        txt = raw.decode('utf-8')
+        for c in txt:
+            char = Character(c)
+            self.assertTrue(char.is_tachkil())
+
+    def test_is_shadda(self):
+        raw = 'ّ'
+        txt = raw.decode('utf-8')
+        for c in txt:
+            char = Character(c)
+            self.assertTrue(char.is_shadda())
+
+    def test_can_be_connected(self):
+        raw = 'ئبتثجحخسشصضطظعغـفقكلمني'
+        txt = raw.decode('utf-8')
+        i = 1
+        while i < len(txt):
+            c = Character(txt[i])
+            p = Character(txt[i-1])
+            i += 1
+            self.assertTrue(c.can_be_connected_to(p))
+
+        raw = 'ءآأؤإاةدذرزوx123#'
+        txt = raw.decode('utf-8')
+        i = 1
+        while i < len(txt):
+            c = Character(txt[i])
+            p = Character(txt[i-1])
+            i += 1
+            self.assertFalse(c.can_be_connected_to(p))
+
+    def test_transform_char(self):
+        raw = 'ل'
+        txt = raw.decode('utf-8')
+        char = Character(txt)
+        self.assertEquals('ﻝ'.decode('utf-8'), char.transform_char())
+
+        raw = 'ل'
+        txt = raw.decode('utf-8')
+        char = Character(txt)
+        char.shape = LetterShape.ISOLATED
+        self.assertEquals('ﻝ'.decode('utf-8'), char.transform_char())
+
+        raw = 'ل'
+        txt = raw.decode('utf-8')
+        char = Character(txt)
+        char.shape = LetterShape.INITIAL
+        self.assertEquals('ﻟ'.decode('utf-8'), char.transform_char())
+
+        raw = 'ل'
+        txt = raw.decode('utf-8')
+        char = Character(txt)
+        char.shape = LetterShape.MIDDLE
+        self.assertEquals('ﻠ'.decode('utf-8'), char.transform_char())
+
+        raw = 'ل'
+        txt = raw.decode('utf-8')
+        char = Character(txt)
+        char.shape = LetterShape.FINAL
+        self.assertEquals('ﻞ'.decode('utf-8'), char.transform_char())
+
+        raw = 'و'
+        txt = raw.decode('utf-8')
+        char = Character(txt)
+        char.shape = LetterShape.FINAL
+        self.assertEquals('ﻮ'.decode('utf-8'), char.transform_char())
+
+        raw = 'ء'
+        txt = raw.decode('utf-8')
+        char = Character(txt)
+        char.shape = LetterShape.ISOLATED
+        self.assertEquals('ﺀ'.decode('utf-8'), char.transform_char())
+
+        special_chars = '(){}<>[]?'
+        special_chars_transformed = ''
+        for c in special_chars:
+            txt = c.decode('utf-8')
+            char = Character(txt)
+            special_chars_transformed += char.transform_char()
+
+        self.assertEquals(')(}{><][؟'.decode('utf-8'), special_chars_transformed)
+
+        raw = 'z'
+        txt = raw.decode('utf-8')
+        char = Character(txt)
+        self.assertEquals('z', char.transform_char())
+
+    def test_transform_lam_alif_char(self):
+        raw = 'ا'
+        txt = raw.decode('utf-8')
+        char = Character(txt)
+        self.assertEquals('ﻻ'.decode('utf-8'), char.transform_lam_alif_char())
+
+    def test_transform_shadda(self):
+            raw = 'َ'
+            txt = raw.decode('utf-8')
+            char = Character(txt)
+            self.assertEquals('ﱠ'.decode('utf-8'), char.transform_shadda_char())
+
+
+class TestText(unittest.TestCase):
+
+    def test_get_word_list(self):
+        raw = 'aaa bbb ccc ذهب الولد'
+        res = [u'aaa', u' ', u'bbb', u' ', u'ccc', u' ', u'ذهب', u' ', u'الولد']
+        txt = Text(raw)
+        self.assertEquals(res, txt.get_word_list())
+
+    def test_transform_word(self):
+        raw = 'ذهب'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﺐﻫﺫ'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'لُ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﹸﻝ'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'ل'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﻝ'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'ذَهَبَ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﹶﺐﹷﻫﹶﺫ'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'ممَم'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﻢﹷﻤﻣ'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'aaa'
+        txt = raw.decode('utf-8')
+        self.assertEquals('aaa'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'لأ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﻷ'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'لآ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﻵ'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'ملآ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﻶﻣ'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'ذلإ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﻹﺫ'.decode('utf-8'), Text.transform_word(txt))
+
+        # shadda fatha
+        raw = 'َّ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﱠ'.decode('utf-8'), Text.transform_word(txt))
+
+        # fatha shadda
+        raw = 'َّ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﱠ'.decode('utf-8'), Text.transform_word(txt))
+
+        # shadda dhamma
+        raw = 'ُّ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﱡ'.decode('utf-8'), Text.transform_word(txt))
+
+        # shadda kasra
+        raw = 'ِّ'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﱢ'.decode('utf-8'), Text.transform_word(txt))
+
+        raw = 'مِّد'
+        txt = raw.decode('utf-8')
+        self.assertEquals('ﺪﳴﻣ'.decode('utf-8'), Text.transform_word(txt))
+
+    def test_transform_text(self):
+        raw = 'ذهب الولد'
+        txt = Text(raw)
+        self.assertEquals('ﺪﻟﻮﻟﺍ ﺐﻫﺫ'.decode('utf-8'), txt.transform_text())
+
+    def test_text_to_unicode_string(self):
+        raw = 'ذهب الولد'
+        txt = Text(raw)
+        self.assertEquals('\u0630\u0647\u0628\u0020\u0627\u0644\u0648\u0644\u062f', txt.text_to_unicode_string())
+
+if __name__ == '__main__':
+    unittest.main()
